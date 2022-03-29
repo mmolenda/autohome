7a35a11f03ca14be040349d7dbe86d25bb90b6bb
Marcin Molenda <marcin@molenda.eu>
2022-03-30 00:58:59 +0200

    Changing API method

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ from autohome import autohome
 app = Flask(__name__)
 ah = autohome.AutoHome()
 
-@app.route('/ah/<action>')
+@app.route('/ah/<action>', methods=['POST'])
 def autohome(action: str):
     try:
         return getattr(ah, f'command_{action}')()
